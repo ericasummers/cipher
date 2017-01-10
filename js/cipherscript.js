c16a5506d65decfd458eba9d3be9d30974b2581d
Erica Wright and Stella Huayhuaca <stella@>
2017-01-09 16:54:59 -0800

    create cipher.js and complete cipher exercises

---
+++ js/cipherscript.js
@@ -0,0 +1,40 @@
+var sentence = prompt("Please enter a sentence.");
+console.log(sentence);
+
+var capletters = function(){
+  alert(first, last);
+};
+var first = sentence.charAt(0).toUpperCase();
+var last = sentence.charAt(sentence.length-1).toUpperCase();
+capletters();
+
+var capreverse = function(){
+  alert(last, first);
+};
+capreverse();
+
+var capandreverse = function(){
+  alert(first + last);
+  alert(sentence + last + first);
+};
+capandreverse();
+
+var letterdivide = function(){
+  var sum = sentence.length;
+  var index = sum / 2;
+  var display = sentence.charAt(index);
+  alert(display + sentence);
+};
+letterdivide();
+
+var str= String(letterdivide);
+alert(str);
+var reverse = dunction(){
+  alert(str.split("").reverse().join(""));
+};
+reverse();
+
+var middle = sentence.substring(1, sentence.length-1);
+[firstcap, lastcap = [lastcap, firstcap];
+
+alert((firstcap + lastcap) + (lastcap + firstcap));
